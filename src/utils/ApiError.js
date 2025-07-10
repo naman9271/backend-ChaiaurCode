f0ebe79ec3df42426a1896a7bea9067d27ff146f
naman9271 <naman9271@gmail.com>
2025-07-11 04:45:06 +0530

    api handling

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message="Something Went Wrong",
+        errors =[],
+        statck=""
+    ){
+        super(message)
+        this.statusCode=statusCode
+        this.success=false
+        this.message=message
+        this.data=null
+        this.errors=errors
+        if(statck){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// high-order function
+//async-handler for try-catch
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+// export {asyncHandler}
+
+//async-handler for promises
+const asyncHandler = (fn)=>(req,res,next)=>{
+    Promise.resolve(fn(req,res,next)).catch(err=>next(err))
+}
+
+export {asyncHandler}
