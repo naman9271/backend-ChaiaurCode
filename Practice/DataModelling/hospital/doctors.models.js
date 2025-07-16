e66ec266628ddefcd9b4a26e1583ea756c185f7f
naman9271 <namanjain9271@gmail.com>
2025-07-17 01:19:14 +0530

    data modelling practice done

---
+++ Practice/DataModelling/hospital/doctors.models.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { Hospital } from "./hospital.models";
+
+const doctorSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        },
+        salary:{
+            type: String,
+            reuired: true
+        },
+        Qualification:{
+            type: String,
+            reuired: true
+        },
+        experienceInYears:{
+            type: Number,
+            reuired: true,
+            default:0
+        },
+        workInHospitals: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: Hospital
+            }
+        ]
+
+    },{timestamps: true}
+)
+
+export const Doctor = mongoose.model("Doctor", doctorSchema)
