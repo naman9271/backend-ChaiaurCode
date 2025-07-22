c59ce1ae3be0206db51c14f4542db0fadc3dc17b
naman9271 <namanjain9271@gmail.com>
2025-07-23 02:57:16 +0530

    jwt and bcrypt and access and refresh token

--- Practice/DataModelling/hospital/patient.models.js
+++ Practice/DataModelling/hospital/patient.models.js
@@ -1,37 +1,38 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const patientSchema = new mongoose.Schema(
-    {
-        name:{
-            type: String,
-            required: true
-        },
-        diagonisedWith:{
-            type: String,
-            required: true
-        },
-        address:{
-            type: String,
-            required: true
-        },
-        age:{
-            type: Number,
-            required: true
-        },
-        BloodGroup:{
-            type: String,
-            required: true
-        },
-        Gender :{
-            type: String,
-            enum: ["M","F","O"],
-            required: true
-        },
-        admittedIn:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref: Hospital
-        }
-    },{timestamps : true}
-)
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    diagonisedWith: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    BloodGroup: {
+      type: String,
+      required: true,
+    },
+    Gender: {
+      type: String,
+      enum: ['M', 'F', 'O'],
+      required: true,
+    },
+    admittedIn: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Hospital,
+    },
+  },
+  { timestamps: true }
+);
 
-export const Patient = mongoose.model("Patient",patientSchema)
+export const Patient = mongoose.model('Patient', patientSchema);
---
+++ README.md
@@ -0,0 +1,14 @@
+#### Doubt
+
+- **Lecture 8**: API Responses and Error handling High Order Function
+- **Lecture 9**: 
+   - aggregation queries in mongoose ,, package - mongoose-aggregate-paginate-v2
+   - bcrypt library - hasing passwords
+   - jsonwebtokens - 
+   - used by pre hook in mongoogse -- data save hone se just pehle ye chl jayega
+   jwt ke aage se smjh nhi aaya 39 min acess token refresh token
+   - 
+   - 
+   - 
+   - 
+   - 
